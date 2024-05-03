9856f985500918006316bbaf19511cf664581b7d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-03 12:58:51 -0600

    Add unit test for get all employees information

--- src/test/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepositoryTests.java
+++ src/test/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepositoryTests.java
@@ -1,9 +1,12 @@
 package dev.angelcruzl.springboot.testing.repository;
 
 import dev.angelcruzl.springboot.testing.model.Employee;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -32,5 +35,33 @@ public class EmployeeRepositoryTests {
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
     }
-    
+
+    @DisplayName("JUnit test for get all employees operation")
+    @Test
+    public void givenEmployeesList_whenFindAll_thenEmployeesList() {
+        // given - precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("Angel")
+                .lastName("Cruz")
+                .email("me@angelcruzl.dev")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john@doe")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // when - action or the behaviour that we are going test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+
+    }
+
 }
