317feb5977efe9e0becb2ff5d4bb2137e7f19215
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-03 16:49:30 -0600

    Add unit test for jpql query with named params

--- src/main/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepository.java
+++ src/main/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package dev.angelcruzl.springboot.testing.repository;
 import dev.angelcruzl.springboot.testing.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +12,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("SELECT e FROM Employee e WHERE e.firstName = ?1 AND e.lastName = ?2")
     Employee findByJPQL(String firstName, String lastName);
+
+    @Query("SELECT e FROM Employee e WHERE e.firstName = :firstName AND e.lastName = :lastName")
+    Employee findByJPQLNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
--- src/test/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepositoryTests.java
+++ src/test/java/dev/angelcruzl/springboot/testing/repository/EmployeeRepositoryTests.java
@@ -161,4 +161,22 @@ public class EmployeeRepositoryTests {
         assertThat(employeeDb).isNotNull();
     }
 
+    @DisplayName("JUnit test for get employee by first name and last name operation using named params")
+    @Test
+    public void givenEmployeeFirstNameAndLastName_whenFindByJPQLNamedParams_thenReturnEmployeeObject() {
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Angel")
+                .lastName("Cruz")
+                .email("me@angelcruzl.dev")
+                .build();
+        employeeRepository.save(employee);
+
+        // when - action or the behaviour that we are going test
+        Employee employeeDb = employeeRepository.findByJPQLNamedParams(employee.getFirstName(), employee.getLastName());
+
+        // then - verify the output
+        assertThat(employeeDb).isNotNull();
+    }
+
 }
