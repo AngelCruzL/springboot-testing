5c50b8ac74a339ced8c0dff8d53475b4361b066d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-04 10:36:06 -0600

    Set up testcontainers at integration test

--- src/test/java/dev/angelcruzl/springboot/testing/integration/EmployeeControllerITests.java
+++ src/test/java/dev/angelcruzl/springboot/testing/integration/EmployeeControllerITests.java
@@ -9,8 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,16 +28,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Testcontainers
 public class EmployeeControllerITests {
 
+    @Container
+    private static final MySQLContainer mySQLContainer = new MySQLContainer("mysql:8");
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private EmployeeRepository repository;
-
     @Autowired
     private ObjectMapper objectMapper;
+
+    @DynamicPropertySource
+    private static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
     @BeforeEach
     public void setUp() {
