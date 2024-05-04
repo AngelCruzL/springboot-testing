1e83ef9350899d10a40050b3511cb379e22af2c1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-03 20:33:57 -0600

    Add createEmployee endpoint functionality & test

---
+++ src/main/java/dev/angelcruzl/springboot/testing/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package dev.angelcruzl.springboot.testing.controller;
+
+import dev.angelcruzl.springboot.testing.model.Employee;
+import dev.angelcruzl.springboot.testing.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+}
