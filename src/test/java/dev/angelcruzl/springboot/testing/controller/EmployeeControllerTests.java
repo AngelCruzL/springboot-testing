35928413191e7c9b2a124033a968bd7bd4d8c1e5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-03 20:54:22 -0600

    Refactor to static imports

--- src/test/java/dev/angelcruzl/springboot/testing/controller/EmployeeControllerTests.java
+++ src/test/java/dev/angelcruzl/springboot/testing/controller/EmployeeControllerTests.java
@@ -3,24 +3,25 @@ package dev.angelcruzl.springboot.testing.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.angelcruzl.springboot.testing.model.Employee;
 import dev.angelcruzl.springboot.testing.service.EmployeeService;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(EmployeeController.class)
 public class EmployeeControllerTests {
@@ -43,7 +44,7 @@ public class EmployeeControllerTests {
                 .email("me@angelcruzl.dev")
                 .build();
 
-        BDDMockito.given(service.saveEmployee(ArgumentMatchers.any(Employee.class)))
+        given(service.saveEmployee(any(Employee.class)))
                 .willAnswer((invocation) -> invocation.getArgument(0));
 
         // when - action or the behaviour that we are going test
@@ -52,10 +53,10 @@ public class EmployeeControllerTests {
                 .content(objectMapper.writeValueAsString(employee)));
 
         // then - verify the result or output using assert statements
-        response.andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+        response.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
     }
 
     @Test
@@ -73,15 +74,15 @@ public class EmployeeControllerTests {
                 .email("john@doe")
                 .build());
 
-        BDDMockito.given(service.getAllEmployees()).willReturn(listOfEmployees);
+        given(service.getAllEmployees()).willReturn(listOfEmployees);
 
         // when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(get("/api/v1/employees"));
 
         // then - verify the result or output using assert statements
-        response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(listOfEmployees.size())));
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(listOfEmployees.size())));
     }
 
 }
